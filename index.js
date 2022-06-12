4cc5133a73bf57bf43eb7fa7ba57614b0cf6bd28
Anmol Jain <anmoljain987@gmail.com>
2022-06-12 22:16:17 +0530

    again

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const { ApolloServer } = require("apollo-server");
-require("dotenv/config");
 const mongoose = require("mongoose");
 const typeDefs = require("./typeDefs");
 const path = require("path");
-
 const resolvers = require("./resolvers/resolvers");
+require("dotenv/config");
 
 const PORT = process.env.PORT || 2000;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
 mongoose
   .connect(process.env.DB_LINK, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
